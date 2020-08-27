3ae5bd9787faae3808744ea7fe376d4a07dae64b
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-08-27 15:48:37 +0100

    Swapped to 5 kafka broker nodes.

--- get-consumer-lag.sh
+++ get-consumer-lag.sh
@@ -1,2 +1,3 @@
 BURROW_IP=`./get-burrow-external-ip.sh`
-curl http://$BURROW_IP:8000/v3/kafka/mykafka/consumer/$1/lag
+#curl http://$BURROW_IP:8000/v3/kafka/mykafka/consumer/$1/lag
+curl http://$BURROW_IP:8000/v3/kafka/mykafka/consumer//lag
